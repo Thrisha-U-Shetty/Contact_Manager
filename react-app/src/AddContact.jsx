aa98f7b89fd722730cdd05683de1617db03014bf
Thrisha-U-Shetty <thrishashetty009@gmail.com>
2025-04-11 21:19:32 +0530

    upadated app

--- react-app/src/AddContact.jsx
+++ react-app/src/AddContact.jsx
@@ -1,31 +1,45 @@
-function AddContact()
+import React from "react";
+class AddContact extends React.Component 
 {
-  state ={
+  state = {
     name:"",
-    email:""
+    email:"",
   };
-  return (
 
-    <>
+   add = (e)=>
+  {
+     e.preventDefault();
+
+     if(this.state.name == "" || this.state.email == "")
+     {
+         alert("All the fields are mandatory !!")
+         return
+     }
+     this.props.addContactHandler(this.state)
+     this.setState({name :"", email:""});
+  }
+  render(){
+  return (
     
       <div className="AddContact">
         <h2>ADD CONTACT : </h2>
-        <form className="form">
+        <form className="form" onSubmit={this.add}>
         <div className="name">
            <label>Name : </label>
-           <input type="text" name="name" placeholder="Type your name here" onChange={(e)=>
-     this.setState({name : e.target.value})
-           }></input>
+           <input type="text" name="name" placeholder="Type your name here" value={this.state.name} onChange={(e)=>
+     this.setState({name : e.target.value})}></input>
         </div>
 
         <div className="email">
            <label>Email : </label>
-           <input type="email" name="email" placeholder="Type your email here"></input>
+           <input type="email" name="email" placeholder="Type your email here" value={this.state.email} onChange={(e)=>
+     this.setState({email : e.target.value})}></input>
         </div>
           <button className="btn">Add</button>
         </form>
       </div>
-    </>
+   
   );
+}
 }
 export default AddContact;
--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css'
-import React , {useState} from 'react'
+import React , {useState , useEffect} from 'react'
 import Header from './Header.jsx'
 import AddContact from './AddContact.jsx'
 import ContactList from './ContactList.jsx'
 
 function App() {
    const [contacts , setContacts] =useState([]);
+   
+   const addContactHandler = (contact) =>{
+      console.log(contact);
+      setContacts([...contacts , contact])
+   }
 
   return (
   <>
      <Header/>
-     <AddContact/>
+     <AddContact addContactHandler={addContactHandler}/>
      <ContactList contacts={contacts}/>
   </>
   )
